b0277b5621ac42a0828d64686343070c2ee161dd
Agathe <agathe.mariller@hispeed.ch>
2025-06-11 23:18:21 +0200

    ajout d'un set de questions

---
+++ questions.js
@@ -0,0 +1,42 @@
+const questions = [
+  {
+    question: "Les applications de suivi du cycle menstruel permettent de prédire le prochain cycle avec exactitude",
+    reponses: ["Vrai", "Faux"],
+    bonne: 1
+  },
+  {
+    question: "On ne peut rien faire d autre que prendre un médicament pour soulager les douleurs liées aux règles.",
+    reponses: ["Vrai", "Faux"],
+    bonne: 1
+  },
+  {
+    question: "Il existe que les tampons comme protections menstruelles",
+    reponses: ["Vrai", "Faux"],
+    bonne: 1
+  },
+    {
+    question: "Les menstruations sont largement représentées dans les médias/film/vidéo",
+    reponses: ["Vrai", "Faux"],
+    bonne: 1
+  },
+    {
+    question: "Il est possible de tomber enceint.e.x si l on a un rapport non protégé pendant ses règles.",
+    reponses: ["Vrai", "Faux"],
+    bonne: 0
+  },
+    {
+    question: "La quantité de sang qui s écoule sur un cycle équivaut à une tasse à café",
+    reponses: ["Vrai", "Faux"],
+    bonne: 0
+  },
+    {
+    question: "Il est possible de tomber enceint.e.x dès les toutes premières règles",
+    reponses: ["Vrai", "Faux"],
+    bonne: 0
+  },
+    {
+    question: "C est normal de se sentir fatigué.e.x à l approche des règles",
+    reponses: ["Vrai", "Faux"],
+    bonne: 1
+  }
+];
